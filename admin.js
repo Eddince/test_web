1933e52696ffc01b75b7fef208427722b4b15f9d
Eddince <126356013+Eddince@users.noreply.github.com>
2025-03-18 18:46:50 -0400

    admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // admin.js
-const API_URL = 'https://backend-mongo-1udy.onrender.com'; // URL de tu backend en Render
+const API_URL = 'https://backend-mongo-1us4.onrender.com'; // URL de tu backend en Render
 
 let datosClientes = [];
 
